028ff5e1be9cf855e9035716c263273fcbf09c40
Samuel <samuelvvelzen@icloud.com>
2025-08-20 11:37:52 +0200

    add dependency  to airportceomodloader

--- Plugin.cs
+++ Plugin.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 namespace ShortcutCeo;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("org.airportceomodloader.humoresque")]
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
